4d6f7eccd2f5759e5aa360349a16f359c796d94b
Ubuntu <ubuntu@ip-172-31-12-105.eu-north-1.compute.internal>
2023-05-19 11:22:29 +0000

    first code

---
+++ string.java
@@ -0,0 +1,11 @@
+[Ipublic class max {
+    public static void main(String[] args) {
+        String str = "Welcome to Javatpoint portal";
+        int strLength = str.length();
+        // Fetching first character
+        System.out.println("Character at 0 index is: "+ str.charAt(0));
+        // The last Character is present at the string length-1 index
+        System.out.println("Character at last index is: "+ str.charAt(strLength-1));
+    }
+}
+
